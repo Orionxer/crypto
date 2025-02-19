d0aa5a0d56c13b83e1c3e00d644afe3244d2e84f
Beancurd <beancurd-jr@jeritech.com>
2025-02-19 17:32:52 +0800

    ✨ 增加资金费率

---
+++ tool/README.md
@@ -0,0 +1,24 @@
+﻿# 说明
+
+> 补充原理：现货与价格的差价
+
+- [ ] [okx文档地址](https://www.okx.com/docs-v5/zh/#overview) 
+- [ ] [binance文档地址]() 
+
+## 环境
+```sh
+pip install python-okx rich
+```
+
+## 待办
+- [ ] 模拟盘开仓/平仓
+- [ ] 请求欧易历史资金费率
+- [ ] 请求币安资金费率
+- [ ] 对比欧易/币安历史资金费率
+- [ ] 考虑加入其他交易所进行对比
+- [ ] 考虑资金费率价差，手续费以及滑点
+
+## 参考资料
+- [聊聊资金费率套利
+](https://www.binance.com/zh-CN/square/post/14411311343169)
+- [资金费率套利策略](https://developer.volcengine.com/articles/7385389759201280026)
---
+++ tool/funding_rate.py
@@ -0,0 +1,30 @@
+﻿import okx.PublicData as PublicData
+import json
+from rich import print_json
+
+# flag = "0"  # 实盘:0 , 模拟盘：1
+
+# publicDataAPI = PublicData.PublicAPI(flag=flag)
+
+# # 获取交易产品基础信息
+# result = publicDataAPI.get_instruments(
+#     instType="SWAP"
+# )
+# # 打印完整json
+# json_str = json.dumps(result)
+# print_json(json_str)
+
+# # 获取所有产品ID
+# inst_ids = [item["instId"] for item in result.get("data", [])]
+# print(json.dumps(inst_ids, indent=4, ensure_ascii=False))
+
+flag = "0"  # 实盘:0 , 模拟盘：1
+
+publicDataAPI = PublicData.PublicAPI(flag=flag)
+
+# 获取永续合约当前资金费率
+result = publicDataAPI.get_funding_rate(
+    instId="IP-USDT-SWAP",
+)
+json_str = json.dumps(result)
+print_json(json_str)
