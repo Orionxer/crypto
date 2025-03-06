151b624fd91908f879e5d051d50b4bd9e8154fb5
Orionxer <orionxer@gmail.com>
2025-03-06 17:18:07 +0800

    ✨ 实现查询账户信息

---
+++ tool/get_account.py
@@ -0,0 +1,42 @@
+﻿import okx.Account as Account
+from rich import print_json
+################### API KEY初始化 ###########################
+import sys, json
+from pathlib import Path
+
+apikey = ""
+secretkey = ""
+passphrase = ""
+
+# 读取API Key等内容
+def read_api_key_data():
+    current_dir = Path.cwd()
+    for file in current_dir.rglob("api_key.json"):
+        # print(f"Found: {file}")
+        try:
+            with open(file, "r", encoding="utf-8-sig") as f:
+                data = json.load(f)
+                # print("Parsed JSON:", data)
+                return data
+        except Exception as e:
+            print(f"Error parsing JSON: {e}")
+            sys.exit(1)
+    print("api_key.json not found")
+    sys.exit(1)
+
+# 读取API Key等内容
+api_key_data = read_api_key_data()
+# 赋值本地变量
+apikey = api_key_data['apikey']
+secretkey = api_key_data['secretkey']
+passphrase = api_key_data['passphrase']
+#############################################################
+
+flag = "0"  # 实盘:0 , 模拟盘:1
+
+accountAPI = Account.AccountAPI(apikey, secretkey, passphrase, False, flag)
+# 查看账户余额
+result = accountAPI.get_account_balance()
+# 查看持仓信息
+json_str = json.dumps(result)
+print_json(json_str)
---
+++ tool/get_balance.py
@@ -0,0 +1,41 @@
+﻿import okx.Funding as Funding
+from rich import print_json
+################### API KEY初始化 ###########################
+import sys, json
+from pathlib import Path
+
+apikey = ""
+secretkey = ""
+passphrase = ""
+
+# 读取API Key等内容
+def read_api_key_data():
+    current_dir = Path.cwd()
+    for file in current_dir.rglob("api_key.json"):
+        # print(f"Found: {file}")
+        try:
+            with open(file, "r", encoding="utf-8-sig") as f:
+                data = json.load(f)
+                # print("Parsed JSON:", data)
+                return data
+        except Exception as e:
+            print(f"Error parsing JSON: {e}")
+            sys.exit(1)
+    print("api_key.json not found")
+    sys.exit(1)
+
+# 读取API Key等内容
+api_key_data = read_api_key_data()
+# 赋值本地变量
+apikey = api_key_data['apikey']
+secretkey = api_key_data['secretkey']
+passphrase = api_key_data['passphrase']
+#############################################################
+
+flag = "0"  # 实盘: 0, 模拟盘: 1
+# 读取资金账户
+fundingAPI = Funding.FundingAPI(apikey, secretkey, passphrase, False, flag)
+# 获取资金账户余额
+result = fundingAPI.get_balances()
+json_str = json.dumps(result)
+print_json(json_str)
