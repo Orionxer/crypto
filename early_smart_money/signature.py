017715e95c66b5a0cbf145cc6b5ce83ec4394651
Orionxer <orionxer@gmail.com>
2025-06-06 17:23:45 +0800

    ✨ 增加查询交易信息

---
+++ early_smart_money/signature.py
@@ -0,0 +1,23 @@
+﻿import requests
+
+def get_solana_transaction(tx_hash):
+    url = "https://api.mainnet-beta.solana.com"
+    payload = {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "getTransaction",
+        "params": [
+            tx_hash,
+            {
+                "encoding": "json",
+                "maxSupportedTransactionVersion": 0
+            }
+        ]
+    }
+    response = requests.post(url, json=payload)
+    return response.json()
+
+# 使用示例
+tx_hash = "GF5tJVe6PZV2DFVhSYRZQSxisoH9YS8fTnGGwBqNcCYJ1jmyBr5VRVcKJRJRsK9TRsyrHmX7K1eEvqgPPvXxSBk"
+result = get_solana_transaction(tx_hash)
+print(result)
---
+++ source/_posts/early-smart-money.md
@@ -0,0 +1,21 @@
+---
+title: SOL早期聪明钱包
+categories:
+  - default
+tags:
+  - default
+banner_img: /images/default.jpg
+index_img: /images/default.jpg
+date: 2025-06-05 14:54:01
+---
+
+> 根据Shanks的[MEME市场擒龙术五：打新盘的真正秘法，P小将们秘而不宣的逻辑](https://x.com/Shanks_A9z/status/1904836990534901969)推文，尝试分析链上知名的聪明钱包的早前聪明钱包或是其小号。
+
+- [ ] 整理完成后尝试跟单测试
+
+以SOL为例，在选择时间筛选数据时，记得保持时间一致性，比如均为**UTC**
+
+### DNF早期聪明钱包或小号
+```sh
+
+```
