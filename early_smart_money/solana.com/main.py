e395f2f58a5e0e06ee488a359b1d262d679d6a24
Orionxer <orionxer@gmail.com>
2025-06-17 17:34:04 +0800

    ✨ 增加Unix时间转换

--- early_smart_money/solana.com/main.py
+++ early_smart_money/solana.com/main.py
@@ -7,6 +7,7 @@ from rich import print
 from rich.syntax import Syntax
 import json
 import time
+from datetime import datetime, timezone
 
 # 导入外部接口
 from getSignaturesForAddress import get_signatures_for_address_list
@@ -19,9 +20,13 @@ signature = "GF5tJVe6PZV2DFVhSYRZQSxisoH9YS8fTnGGwBqNcCYJ1jmyBr5VRVcKJRJRsK9TRsy
 token_address = "ENfpbQUM5xAnNP8ecyEQGFJ6KwbuPjMwv7ZjR29cDuAb"
 # 查询列表
 results = get_signatures_for_address_list(token_address, signature)
-# 打印列表
+# 打印列表 
+# TODO 添加本地超时
+# TODO 添加容错处理
 for i, (block_time, signature) in enumerate(results, start=1):
     # 获取Signer地址
     signer = get_signer(signature)
-    print(f"[{i:03d}] blockTime: {block_time}, signature: {signature}, signer: {signer}")
+    # 转换为可读的UTC时间 # ! 默认0时区，即 +UTC
+    human_time = datetime.fromtimestamp(block_time, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+    print(f"[{i:03d}] BlockTime: {block_time}, HumanTime: {human_time}, Signature: {signature}, Signer: {signer}")
     time.sleep(5)
